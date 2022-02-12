e91a169ae4421063cc796b66007f3c6c057381a3
siddhant <siddhantsaraf.91@gmail.com>
2022-02-12 13:59:48 +0530

    ui for user

---
+++ lib/User/Screens/bookticket.dart
@@ -0,0 +1,130 @@
+import 'package:dropdown_formfield/dropdown_formfield.dart';
+import 'package:flutter/material.dart';
+
+class BookTicket extends StatefulWidget {
+  const BookTicket({Key? key}) : super(key: key);
+
+  @override
+  _BookTicketState createState() => _BookTicketState();
+}
+
+class _BookTicketState extends State<BookTicket> {
+
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController name = TextEditingController();
+  TextEditingController phno = TextEditingController();
+  TextEditingController tquantity = TextEditingController();
+  TextEditingController movie  = TextEditingController();
+  late String _myActivity;
+  late String _myActivityResult;
+
+  @override
+  void initState() {
+    super.initState();
+    _myActivity = '';
+    _myActivityResult = '';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Book Ticket'),),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+
+              controller: name,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+
+              controller: phno,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: DropDownFormField(
+                titleText: 'My workout',
+                hintText: 'Please choose one',
+                value: _myActivity,
+                onSaved: (value) {
+                  setState(() {
+                    _myActivity = value;
+                  });
+                },
+                onChanged: (value) {
+                  setState(() {
+                    _myActivity = value;
+                  });
+                },
+                dataSource: [
+                  {
+                    "display": "Running",
+                    "value": "Running",
+                  },
+                  {
+                    "display": "Climbing",
+                    "value": "Climbing",
+                  },
+                  {
+                    "display": "Walking",
+                    "value": "Walking",
+                  },
+                  {
+                    "display": "Swimming",
+                    "value": "Swimming",
+                  },
+                  {
+                    "display": "Soccer Practice",
+                    "value": "Soccer Practice",
+                  },
+                  {
+                    "display": "Baseball Practice",
+                    "value": "Baseball Practice",
+                  },
+                  {
+                    "display": "Football Practice",
+                    "value": "Football Practice",
+                  },
+                ],
+                textField: 'display',
+                valueField: 'value',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  setState(() {
+    _myActivityResult = _myActivity;
+    });
+    }
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+
+                  );
+
+                }, child: const Text("book now"),
+
+            ),
+          ],
+        ),
+
+      ),
+    );
+  }
+}
