e55450b70ec5ccda9da2b0f8218050266952e021
siddhant <siddhantsaraf.91@gmail.com>
2022-02-12 14:36:48 +0530

    add screen for admin 2

--- lib/Admin/Screens/adminhome.dart
+++ lib/Admin/Screens/adminhome.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:titest/Admin/model/movie.dart';
+
 class AdminHome extends StatefulWidget {
   const AdminHome({Key? key}) : super(key: key);
 
@@ -7,106 +10,87 @@ class AdminHome extends StatefulWidget {
 }
 
 class _AdminHomeState extends State<AdminHome> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController name = TextEditingController();
+  TextEditingController phno = TextEditingController();
+  TextEditingController tquantity = TextEditingController();
+  TextEditingController tprice = TextEditingController();
+  late String _movie;
+  late String _movieresult;
+  List<Movie> movielist = [
+    Movie("one", 200, '10/2/2022', 20),
+    Movie('two', 250, '12/2/2022', 30),
+    Movie('three', 300, '/13/2/2022', 40),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Admin Home'),),
-       body: Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          TextFormField(
-
-            controller: name,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-
-            controller: phno,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          Container(
-            padding: EdgeInsets.all(16),
-            child: DropDownFormField(
-              titleText: 'My workout',
-              hintText: 'Please choose one',
-              value: _myActivity,
-              onSaved: (value) {
-                setState(() {
-                  _myActivity = value;
-                });
-              },
-              onChanged: (value) {
-                setState(() {
-                  _myActivity = value;
-                });
-              },
-              dataSource: [
-                {
-                  "display": "Running",
-                  "value": "Running",
-                },
-                {
-                  "display": "Climbing",
-                  "value": "Climbing",
-                },
-                {
-                  "display": "Walking",
-                  "value": "Walking",
-                },
-                {
-                  "display": "Swimming",
-                  "value": "Swimming",
-                },
-                {
-                  "display": "Soccer Practice",
-                  "value": "Soccer Practice",
-                },
-                {
-                  "display": "Baseball Practice",
-                  "value": "Baseball Practice",
-                },
-                {
-                  "display": "Football Practice",
-                  "value": "Football Practice",
-                },
-              ],
-              textField: 'display',
-              valueField: 'value',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // Validate returns true if the form is valid, or false otherwise.
-              if (_formKey.currentState!.validate()) {
-                // If the form is valid, display a snackbar. In the real world,
-                // you'd often call a server or save the information in a database.
-                setState(() {
-                  _myActivityResult = _myActivity;
-                });
-              }
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Processing Data')),
-
-              );
-
-            }, child: const Text("book now"),
-
-          ),
-        ],
+      appBar: AppBar(
+        title: Text('Admin Home'),
       ),
-
-    ),
+      body: Form(
+        key: _formKey,
+        child:ListView.builder(
+          itemCount: movielist.length,
+            itemBuilder: (context,index){
+           return Column(
+             children: [
+               TextFormField(
+                 decoration: InputDecoration(
+                   label: Text('${movielist[index].moviename}'),
+                 ),
+                 controller: name,
+                 validator: (value) {
+                   if (value == null || value.isEmpty) {
+                     return 'Please enter some text';
+                   }
+                   return null;
+                 },
+                 onChanged: (value){
+                   setState(() {
+                     value = name.text;
+                   });
+                 },
+               ),
+               TextFormField(
+                 decoration: InputDecoration(
+                   label: Text('${movielist[index].tquant}'),
+                 ),
+                 controller: tquantity,
+                 validator: (value) {
+                   if (value == null || value.isEmpty ) {
+                     return 'Please enter some text';
+                   }
+                   return null;
+                 },
+                 onChanged: (value){
+                   setState(() {
+                     value = tquantity.text;
+                   });
+                 },
+               ),
+               TextFormField(
+                 decoration: InputDecoration(
+                   label: Text('${movielist[index].tprice}'),
+                 ),
+                 controller: tprice,
+                 validator: (value) {
+                   if (value == null || value.isEmpty ) {
+                     return 'Please enter some text';
+                   }
+                   return null;
+                 },
+                 onChanged: (value){
+                   setState(() {
+                     value = tprice.text;
+                   });
+                 },
+               ),
+             ],
+           );
+        })
+      ),
     );
   }
 }
---
+++ lib/Admin/model/movie.dart
@@ -0,0 +1,7 @@
+class Movie{
+  final String moviename;
+  final int tquant;
+  final String time;
+  final int tprice;
+  Movie(this.moviename, this.tquant, this.time, this.tprice);
+}
